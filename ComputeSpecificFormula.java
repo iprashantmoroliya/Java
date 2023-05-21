57a136723b0fe4c8a41edb36365649cd83649587
Nine Tail Fox <131880994+iprashantmoroliya@users.noreply.github.com>
2023-05-21 08:51:59 +0530

    Compute Specific Formula

---
+++ ComputeSpecificFormula.java
@@ -0,0 +1,10 @@
+/*
+    Write a Java program to compute a specified formula. 
+    4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11))
+*/
+
+class ComputeSpecificFormula {
+    public static void main(String[] args) {
+        System.out.println(4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11)));
+    }
+}
