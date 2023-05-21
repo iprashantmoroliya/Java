f33ca8113ddf6389bc2fa5139874a299c66e1faa
Nine Tail Fox <131880994+iprashantmoroliya@users.noreply.github.com>
2023-05-21 08:49:20 +0530

    Print Specified Expression

---
+++ PrintSpecifiedExpression.java
@@ -0,0 +1,10 @@
+/*
+    Write a Java program to compute the specified expressions and print the output. 
+    ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)) 
+*/
+
+class PrintSpecifiedExpression {
+    public static void main(String[] args) {
+        System.out.println(((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)));
+    }
+}
